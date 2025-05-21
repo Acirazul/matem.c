66a98fb60d097ae6604779125876034252ce0f4b
acirazul <gerziakelly@hotmail.com>
2025-05-21 01:49:54 +0000

    teste

--- matem.c
+++ matem.c
@@ -1,3 +1,23 @@
 #include <studio.h>
 
+int main(){
+/*
+Incremento (++)
+Pré-Incremento ++a
+Pós-Incremento a++
+Decremento (--)
+Pré-Decremento --a
+Pós-Decremento a--
+*/
 
+int numero1 = 1;
+
+printf("Antes incremento: %d\n", nuemro1);
+numero1++;
+printf("Apos incremento: %d\n", nuemro1);
+
+
+numero1--;
+printf("Apos decremento: %d\n", nuemro1);
+
+}
