d1cec6ed7a41501df4ca69161a2bf01ed58e22ca
acirazul <gerziakelly@hotmail.com>
2025-05-22 18:13:30 +0000

    inteiro

--- variáveisinteirasepontosflutuante.c
+++ variáveisinteirasepontosflutuante.c
@@ -1,1 +1,18 @@
+#include <stdio.h>;
 
+int main(){
+int a = 10;
+int a b = 3;
+int soma = a + b;
+int diferenca = a - b;
+int produto = a * b;
+int quociente = a / b; // Note que a divisão de inteiros resulta em um numero inteiro
+
+printf("Soma: %d/n", soma);
+printf("Diferença: %d/n", diferenca);
+printf("Produto: %d/n", produto);
+printf("Quociente: %d/n", quociente);
+
+resultado 0:
+
+}
