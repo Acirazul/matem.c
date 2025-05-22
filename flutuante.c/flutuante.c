da90194ba0523ac4ce5b9fa0b18bf2537f7d67d3
acirazul <gerziakelly@hotmail.com>
2025-05-22 18:31:03 +0000

    test

--- flutuante.c/flutuante.c
+++ flutuante.c/flutuante.c
@@ -5,8 +5,12 @@ float x = 5.5;
 float y = 2.2;
 float soma = x + y;
 float diferenca = x -Y;
-float Quaociente = x / y; // Divisão de pontos flutuantes 
+float quociente = x / y; // Divisão de pontos flutuantes 
 
+prinf("Soma: %.2f/n", soma);
+prinf("Diferença: %.2f/n", diferenca);
+prinf("Quociente: %.2f/n", quociente);
 
+return 0;
 
 }
