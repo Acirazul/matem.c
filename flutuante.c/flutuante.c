254c1c183f50477b8f43f6579dc7424d43268e11
acirazul <gerziakelly@hotmail.com>
2025-05-22 18:32:32 +0000

    test

--- flutuante.c/flutuante.c
+++ flutuante.c/flutuante.c
@@ -4,12 +4,12 @@ int main(){
 float x = 5.5;
 float y = 2.2;
 float soma = x + y;
-float diferenca = x -Y;
+float diferenca = x - y;
 float quociente = x / y; // Divisão de pontos flutuantes 
 
-prinf("Soma: %.2f/n", soma);
-prinf("Diferença: %.2f/n", diferenca);
-prinf("Quociente: %.2f/n", quociente);
+printf("Soma: %.2f/n", soma);
+printf("Diferença: %.2f/n", diferenca);
+printf("Quociente: %.2f/n", quociente);
 
 return 0;
 
