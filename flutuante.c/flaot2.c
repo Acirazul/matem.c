21eb3a4315262cd2853b8d4a954a576a075817d6
acirazul <gerziakelly@hotmail.com>
2025-05-22 18:54:27 +0000

    test

--- flutuante.c/flaot2.c
+++ flutuante.c/flaot2.c
@@ -3,7 +3,7 @@ include <stdio.h>
 int main() {
     int a = 10;
     int b = 3;
-    float quociente = a / b; // 'a' é explicitamente convertido para float
+    float quociente = a / b; 
  
     printf("Quociente: %.2f\n", quociente);
  
